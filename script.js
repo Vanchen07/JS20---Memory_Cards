2efdc8082972910fd6f9203d27d88f24647326a2
Vanessa Chen <vanchen07@gmail.com>
2020-06-04 19:30:59 -0700

    implement function to create card a single card html element and append to card container

--- script.js
+++ script.js
@@ -32,3 +32,31 @@ const cardsData = [
 function createCards() {
     cardsData.forEach((data, index) => createCard(data, index));
 }
+
+function createCard(data, index) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    if (index === 0) {
+        card.classList.add('active');
+    }
+
+    card.innerHTML= `
+        <div class="inner-card">
+            <div class="inner-card-front">
+                <p>
+                    ${data.question}
+                </p>
+            </div>
+            <div class="inner-card-back">
+                <p>
+                    ${data.answer}
+                </p>
+            </div>
+        </div>
+    `;
+
+    cardsEl.push(card);
+
+    cardsContainer.appendChild(card);
+}
