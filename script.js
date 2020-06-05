36d53272251eead5c0eaef6d1cbe5a4b4250c3c4
Vanessa Chen <vanchen07@gmail.com>
2020-06-04 19:08:52 -0700

    grab current element for js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 const cardsContainer = document.getElementById('cards-container');
 const prevBtn = document.getElementById('prev');
 const nextBtn = document.getElementById('next');
+const currentEl = document.getElementById('current');
