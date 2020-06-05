a1caa5548b0e81f8d63b7dffa55aa75abfd640ff
Vanessa Chen <vanchen07@gmail.com>
2020-06-04 19:12:50 -0700

    grab hide element for js

--- script.js
+++ script.js
@@ -3,3 +3,4 @@ const prevBtn = document.getElementById('prev');
 const nextBtn = document.getElementById('next');
 const currentEl = document.getElementById('current');
 const showBtn = document.getElementById('show');
+const hideBtn = document.getElementById('hide');
