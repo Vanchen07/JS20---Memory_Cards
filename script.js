2e41089a33ed8b49772acaf07c6e3d5551f63683
Vanessa Chen <vanchen07@gmail.com>
2020-06-04 19:09:24 -0700

    grab show element for js

--- script.js
+++ script.js
@@ -2,3 +2,4 @@ const cardsContainer = document.getElementById('cards-container');
 const prevBtn = document.getElementById('prev');
 const nextBtn = document.getElementById('next');
 const currentEl = document.getElementById('current');
+const showBtn = document.getElementById('show');
