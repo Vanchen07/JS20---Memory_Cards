8ea9a27fcf2c2641e0c07f8ed174283fcfb448d7
Vanessa Chen <vanchen07@gmail.com>
2020-06-04 19:21:59 -0700

    create variables cards data and cards array

--- script.js
+++ script.js
@@ -9,3 +9,22 @@ const answerEl = document.getElementById('answer');
 const addCardBtn = document.getElementById('add-card');
 const clearBtn = document.getElementById('clear');
 const addContainer = document.getElementById('add-container');
+
+let currentActiveCard = 0;
+
+const cardsEl = [];
+
+const cardsData = [
+    {
+        question: 'What must a variable begin with?',
+        answer: 'A letter, $ or _'
+    },
+    {
+        question: 'What is a variable?',
+        answer: 'Container for a piece of data'
+    },
+    {
+        question: 'Example of a Case Sensitive Variable',
+        answer: 'thisIsAVariable'
+    }
+];
